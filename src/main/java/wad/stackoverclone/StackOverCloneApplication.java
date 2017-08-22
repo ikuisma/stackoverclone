0056b3b9a056e577c650ec143fa49e901c2856ff
ikuisma <ikuisma@Ilkkas-MacBook-Pro.local>
2017-08-23 00:29:30 +0300

    Initial boiler plate project code.

---
+++ src/main/java/wad/stackoverclone/StackOverCloneApplication.java
@@ -0,0 +1,13 @@
+package wad.stackoverclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackOverCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StackOverCloneApplication.class, args);
+	}
+
+}
