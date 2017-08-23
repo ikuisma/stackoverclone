777c7d574c8f48492f7769eaa9c699aaa5e81d14
ikuisma <ikuisma@Ilkkas-MacBook-Pro.local>
2017-08-23 20:24:47 +0300

    Added new placeholder HTML file and default controller methods.

--- src/main/java/wad/stackoverclone/controller/DefaultController.java
+++ src/main/java/wad/stackoverclone/controller/DefaultController.java
@@ -2,16 +2,18 @@ package wad.stackoverclone.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class DefaultController {
 
-    @RequestMapping("/")
-    @ResponseBody
-    public String home() {
-        return "StackOverClone -- coming soon to stores near you!";
+    @RequestMapping("*")
+    public String redirect() {
+        return "redirect:/home";
     }
 
+    @RequestMapping("/home")
+    public String home() {
+        return "index";
+    }
 
 }
