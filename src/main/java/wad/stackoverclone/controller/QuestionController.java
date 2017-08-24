2d24ab2465ccfecb12636b36ab00254b20db4f4d
ikuisma <ikuisma@Ilkkas-MacBook-Pro.local>
2017-08-24 23:54:20 +0300

    Controller for questions.

---
+++ src/main/java/wad/stackoverclone/controller/QuestionController.java
@@ -0,0 +1,16 @@
+package wad.stackoverclone.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/questions")
+public class QuestionController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String get() {
+        return "questions";
+    }
+
+}
