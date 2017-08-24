8124fc5ea7bb9dcb9d8e2796d54abf5d7c6bb55e
ikuisma <ikuisma@Ilkkas-MacBook-Pro.local>
2017-08-25 00:41:14 +0300

    Updated Question controller to add all questions to the model.

--- src/main/java/wad/stackoverclone/controller/QuestionController.java
+++ src/main/java/wad/stackoverclone/controller/QuestionController.java
@@ -1,15 +1,26 @@
 package wad.stackoverclone.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import wad.stackoverclone.service.QuestionService;
 
 @Controller
 @RequestMapping("/questions")
 public class QuestionController {
 
+    private final QuestionService questionService;
+
+    @Autowired
+    public QuestionController(QuestionService questionService) {
+        this.questionService = questionService;
+    }
+
     @RequestMapping(method = RequestMethod.GET)
-    public String get() {
+    public String get(Model model) {
+        model.addAttribute("questions", questionService.getAllQuestions());
         return "questions";
     }
 
