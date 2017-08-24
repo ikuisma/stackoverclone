e5fe55cf2585e7576960546d433d3ff6532226e0
ikuisma <ikuisma@Ilkkas-MacBook-Pro.local>
2017-08-25 00:40:49 +0300

    Added a service for questions.

---
+++ src/main/java/wad/stackoverclone/service/QuestionService.java
@@ -0,0 +1,27 @@
+package wad.stackoverclone.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import wad.stackoverclone.domain.Question;
+import wad.stackoverclone.repository.QuestionRepository;
+
+import java.util.List;
+
+@Service
+public class QuestionService {
+
+    private final QuestionRepository questionRepository;
+
+    @Autowired
+    public QuestionService(QuestionRepository questionRepository) {
+        this.questionRepository = questionRepository;
+    }
+
+    public List<Question> getAllQuestions() {
+        return questionRepository.findAll();
+    }
+
+
+
+
+}
