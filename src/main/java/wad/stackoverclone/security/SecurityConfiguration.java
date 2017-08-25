4cdff90e2fd73406ad4d7eea3b200eaedfd49360
ikuisma <ikuisma@Ilkkas-MacBook-Pro.local>
2017-08-25 08:39:34 +0300

    Added security config to use the new customised login page.

--- src/main/java/wad/stackoverclone/security/SecurityConfiguration.java
+++ src/main/java/wad/stackoverclone/security/SecurityConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import wad.stackoverclone.service.CustomUserDetailsService;
 
 @Configuration
@@ -24,17 +25,19 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity httpSecurity) throws Exception{
-        httpSecurity
-                .authorizeRequests()
+        httpSecurity.authorizeRequests()
                 .anyRequest()
                 .authenticated()
                 .and()
                 .formLogin()
+                .loginPage("/login")
+                .passwordParameter("password")
+                .usernameParameter("username")
                 .permitAll()
                 .and()
                 .logout()
-                .permitAll();
-        httpSecurity.logout().permitAll();
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login");
     }
 
     @Autowired
