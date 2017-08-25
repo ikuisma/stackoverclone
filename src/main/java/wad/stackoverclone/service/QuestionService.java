bcfa1eb4e0fd0653cb013d5ac13d7353646344c3
ikuisma <ikuisma@Ilkkas-MacBook-Pro.local>
2017-08-25 21:16:36 +0300

    Added authorisation as well as new methods to Question service for adding answers to questions.

--- src/main/java/wad/stackoverclone/service/QuestionService.java
+++ src/main/java/wad/stackoverclone/service/QuestionService.java
@@ -1,8 +1,11 @@
 package wad.stackoverclone.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
+import wad.stackoverclone.domain.Answer;
 import wad.stackoverclone.domain.Question;
+import wad.stackoverclone.repository.AnswerRepository;
 import wad.stackoverclone.repository.QuestionRepository;
 
 import javax.transaction.Transactional;
@@ -13,21 +16,38 @@ public class QuestionService {
 
     private final QuestionRepository questionRepository;
     private final AccountService accountService;
+    private final AnswerRepository answerRepository;
 
     @Autowired
-    public QuestionService(QuestionRepository questionRepository, AccountService accountService) {
+    public QuestionService(
+            QuestionRepository questionRepository,
+            AccountService accountService,
+            AnswerRepository answerRepository) {
         this.questionRepository = questionRepository;
         this.accountService = accountService;
+        this.answerRepository = answerRepository;
     }
 
+    @PreAuthorize("isAuthenticated()")
     public List<Question> getAllQuestions() {
         return questionRepository.findAll();
     }
 
     @Transactional
+    @PreAuthorize("isAuthenticated()")
     public void postNewQuestion(Question question) {
         question = this.questionRepository.save(question);
         question.setAuthor(accountService.currentAccount());
     }
+
+    @Transactional
+    @PreAuthorize("isAuthenticated()")
+    public void postNewAnswer(Answer answer, Long questionId) {
+        Question q = questionRepository.findOne(questionId);
+        answer.setAuthor(accountService.currentAccount());
+        answer.setQuestion(q);
+        answerRepository.save(answer);
+    }
+
 
 }
