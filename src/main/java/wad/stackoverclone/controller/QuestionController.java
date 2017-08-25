937eaab05f83084463dc8f3f8a08658fa392b8e1
ikuisma <ikuisma@Ilkkas-MacBook-Pro.local>
2017-08-25 21:17:45 +0300

    Added new mappings to question controller for the individual questions and pages.

--- src/main/java/wad/stackoverclone/controller/QuestionController.java
+++ src/main/java/wad/stackoverclone/controller/QuestionController.java
@@ -5,9 +5,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import wad.stackoverclone.domain.Answer;
 import wad.stackoverclone.domain.Question;
+import wad.stackoverclone.repository.QuestionRepository;
 import wad.stackoverclone.service.QuestionService;
 
 import javax.validation.Valid;
@@ -17,10 +20,12 @@ import java.util.List;
 public class QuestionController {
 
     private final QuestionService questionService;
+    private final QuestionRepository questionRepository;
 
     @Autowired
-    public QuestionController(QuestionService questionService) {
+    public QuestionController(QuestionService questionService, QuestionRepository questionRepository) {
         this.questionService = questionService;
+        this.questionRepository  = questionRepository;
     }
 
     @RequestMapping(value = "/questions", method = RequestMethod.GET)
@@ -45,6 +50,26 @@ public class QuestionController {
     @RequestMapping(value="/ask", method = RequestMethod.GET)
     public String getCreationForm(@ModelAttribute Question question) {
         return "ask";
+    }
+
+    @RequestMapping(value="/questions/{id}", method = RequestMethod.GET)
+    public String getQuestion(@PathVariable Long id, Model model, @ModelAttribute Answer answer) {
+        model.addAttribute("question", questionRepository.findOne(id));
+        return "question";
+    }
+
+    @RequestMapping(value="/questions/{id}/answers", method = RequestMethod.POST)
+    public String postAnswer(
+            @PathVariable Long id,
+            Model model,
+            @Valid @ModelAttribute Answer answer,
+            BindingResult result) {
+        model.addAttribute("question", questionRepository.findOne(id));
+        if (result.hasErrors()) {
+            return "question";
+        }
+        questionService.postNewAnswer(answer, id);
+        return "redirect:/questions/" + id;
     }
 
 }
