ee4ba2ea283d17a34ca2318f67edff53137a939f
ikuisma <ikuisma@Ilkkas-MacBook-Pro.local>
2017-08-25 13:15:15 +0300

    Updated Account class with better validations.

--- src/main/java/wad/stackoverclone/domain/Account.java
+++ src/main/java/wad/stackoverclone/domain/Account.java
@@ -1,25 +1,22 @@
 package wad.stackoverclone.domain;
 
+import org.hibernate.annotations.NaturalId;
+import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotBlank;
-import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.validation.constraints.NotNull;
 
 @Entity
 public class Account extends AbstractPersistable<Long>{
 
     @NotBlank
-    @NotEmpty
-    @NotNull
-    @Column(unique = true)
+    @Length(min = 5, max=30)
+    @NaturalId
     private String username;
 
-    @NotEmpty
-    @NotNull
     @NotBlank
+    @Length(min = 6)
     private String password;
 
     public String getUsername() {
