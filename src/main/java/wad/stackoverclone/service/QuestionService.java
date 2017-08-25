694a49f0abeb9ea8d52248f10c40d4e2b74c3335
ikuisma <ikuisma@Ilkkas-MacBook-Pro.local>
2017-08-25 18:19:37 +0300

    Added functions to the QuestionService for posting new questions.

--- src/main/java/wad/stackoverclone/service/QuestionService.java
+++ src/main/java/wad/stackoverclone/service/QuestionService.java
@@ -5,20 +5,29 @@ import org.springframework.stereotype.Service;
 import wad.stackoverclone.domain.Question;
 import wad.stackoverclone.repository.QuestionRepository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
 public class QuestionService {
 
     private final QuestionRepository questionRepository;
+    private final AccountService accountService;
 
     @Autowired
-    public QuestionService(QuestionRepository questionRepository) {
+    public QuestionService(QuestionRepository questionRepository, AccountService accountService) {
         this.questionRepository = questionRepository;
+        this.accountService = accountService;
     }
 
     public List<Question> getAllQuestions() {
         return questionRepository.findAll();
+    }
+
+    @Transactional
+    public void postNewQuestion(Question question) {
+        question = this.questionRepository.save(question);
+        question.setAuthor(accountService.currentAccount());
     }
 
 }
