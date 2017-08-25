5ffce77c20d02b72f6fb3f0419ced0b6a7c6f56d
ikuisma <ikuisma@Ilkkas-MacBook-Pro.local>
2017-08-25 18:18:14 +0300

    Added functions to AccountService for handling questions related to the account.

--- src/main/java/wad/stackoverclone/service/AccountService.java
+++ src/main/java/wad/stackoverclone/service/AccountService.java
@@ -1,11 +1,16 @@
 package wad.stackoverclone.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.BindingResult;
 import wad.stackoverclone.domain.Account;
+import wad.stackoverclone.domain.Question;
 import wad.stackoverclone.repository.AccountRepository;
+
+import javax.transaction.Transactional;
 
 @Service
 public class AccountService {
@@ -27,6 +32,21 @@ public class AccountService {
 
     public boolean usernameIsTaken(Account account) {
         return (accountRepository.findAccountByUsername(account.getUsername()) != null);
+    }
+
+    @Transactional
+    public void addQuestion(Question q) {
+        Account a = currentAccount();
+        a.getQuestions().add(q);
+        accountRepository.save(a);
+    }
+
+    public Account currentAccount() {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            if (authentication == null || !authentication.isAuthenticated()) {
+                return null;
+            }
+            return accountRepository.findAccountByUsername(authentication.getName());
     }
 
 }
