1a1357f1c51231cc4ab5130db3ce35605bcf0397
ikuisma <ikuisma@Ilkkas-MacBook-Pro.local>
2017-08-25 08:40:44 +0300

    Added login page.

--- src/test/java/wad/stackoverclone/selenium/pageobjects/LoginPage.java
+++ src/test/java/wad/stackoverclone/selenium/pageobjects/LoginPage.java
@@ -8,12 +8,19 @@ import org.openqa.selenium.support.FindBy;
 @PageUrl("/login")
 public class LoginPage extends FluentPage {
 
-    @FindBy(xpath = "//input[@type=\"submit\"]")
+    @FindBy(xpath = "//button[@type=\"submit\"]")
     private FluentWebElement submitButton;
+
+    @FindBy(xpath = ".//form[@action=\"/login\"]")
+    private FluentWebElement loginForm;
 
     public void login(String username, String password) {
         $("input").fill().with(username, password);
         submitButton.click();
+    }
+
+    public boolean loginFormIsVisible() {
+        return loginForm.displayed();
     }
 
 }
